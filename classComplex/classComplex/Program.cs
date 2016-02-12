c81882cdac4bdc1cbe0783e97cac44bdc797e9ee
Аян Курманбай < >
2016-02-12 23:56:05 +0600

    added class complex

---
+++ classComplex/classComplex/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classComplex
+{
+    class Complex
+    {
+        public int a, b;
+        public Complex(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public static Complex operator +(Complex x, Complex y)
+        {
+            Complex z = new Complex(x.a * y.b + y.a * x.b, x.b * y.b);
+            return z;
+        }
+        public override string ToString()
+        {
+            return a + "/" + b;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a1 = int.Parse(Console.ReadLine());
+            int b1 = int.Parse(Console.ReadLine());
+            int a2 = int.Parse(Console.ReadLine());
+            int b2 = int.Parse(Console.ReadLine());
+            Complex sum1 = new Complex(a1, b1);
+            Complex sum2 = new Complex(a2, b2);
+            Complex ans = sum1 + sum2;
+            Console.WriteLine(ans);
+            Console.ReadKey();
+        }
+    }
+}
