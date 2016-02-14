033daeb22173a7336705261970e955e8671d2967
Аян Курманбай < >
2016-02-14 13:19:04 +0600

    added prime numbers

---
+++ primeNumbers/primeNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] a = new int[10001];
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 2; j <= Math.Sqrt(a[i]); j++)
+                {
+                    if (a[i]%j==0 && a[i] != j)
+                    {
+                        a[i] = 0;
+                        break;
+                    }
+                }
+                if (a[i]!=0 && a[i]!=1)
+                {
+                    Console.WriteLine(" " + a[i] + " ");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ stack/stack/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dirTree(@"C:\Users\Lenovo\me");
+        }
+        static void dirTree(string path)
+        {
+            Stack<string> dir = new Stack<string>();
+            dir.Push(path);
+            while (dir.Count > 0)
+            {
+                string[] subDir = Directory.GetDirectories(dir.Pop());
+                foreach (string q in subDir)
+                {
+                    Console.WriteLine(q + ": " + Directory.GetFiles(q).Length);
+                    dir.Push(q);
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
